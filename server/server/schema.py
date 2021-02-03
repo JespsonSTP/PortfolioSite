7011860f35aaaa48fbfbd71596c241e8fdd06047
JespsonSTP <jespstpierre@yahoo.com>
2021-02-03 18:07:18 -0500

    adding graphql auh to my app

--- server/server/schema.py
+++ server/server/schema.py
@@ -1,11 +1,12 @@
 from graphene import Schema, ObjectType
 import djangobackend.schema
+import users.gql.schema
 
 #there a pass because it inherits from the other queries
-class Query(djangobackend.schema.Query, ObjectType):
+class Query(djangobackend.schema.Query, users.gql.schema.Query, ObjectType):
     pass
 
-class Mutation(djangobackend.schema.Mutation, ObjectType):
+class Mutation(djangobackend.schema.Mutation, users.gql.schema.Mutation, ObjectType):
     pass
 
 schema = Schema(query=Query, mutation=Mutation)
